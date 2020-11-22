8dca21413949d04ba2741bad4f8de15105d3a103
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-22 23:09:46 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('guildMemberAdd', member => {
   // if (!channel) return;
   // Send the message, mentioning the member
   //channel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
-  member.guild.channels.get('762158660793008165').send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
+  member.guild.channels.cache.get('762158660793008165').send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
 });
 client.on('message', msg => {
   if (msg.content==='new'){
@@ -39,7 +39,7 @@ client.on('message', msg => {
     if (!channel) return;
     // Send the message, mentioning the member
     channel.send(`${msg.member},한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
-    msg.member.guild.channels.get('772669318085345280').send(`${msg.member}hi.`);
+    msg.member.guild.channels.cache.get('772669318085345280').send(`${msg.member}hi.`);
   }
 });
 client.on('message', msg => {
