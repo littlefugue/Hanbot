b43a8b028101e6980772c2c1715be42a363c9b9b
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-22 22:52:39 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,9 +31,9 @@ client.on('guildMemberAdd', member => {
   channel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
 });
 client.on('message', msg => {
-  if (msg.content==='this'){
+  if (msg.content==='new'){
     // Send the message to a designated channel on a server:
-    const channel = msg.member.guild.channels.cache.find(ch => ch.name === '잡담');
+    const channel = msg.member.guild.channels.cache.find(ch => ch.name === 'sandbox');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
