a2055cfbfac467f645e99b0ce92c6d5df72208f3
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-22 23:04:40 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,11 +24,12 @@ client.on('message', message => {
 });
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === '잡담');
+  // const channel = member.guild.channels.cache.find(ch => ch.name === '잡담');
   // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
+  // if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
+  //channel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
+  member.guild.channels.get('762158660793008165').send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
 });
 client.on('message', msg => {
   if (msg.content==='new'){
@@ -38,6 +39,7 @@ client.on('message', msg => {
     if (!channel) return;
     // Send the message, mentioning the member
     channel.send(`${msg.member},한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
+    msg.member.guild.channels.get('772669318085345280').send(`${msg.member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
   }
 });
 client.on('message', msg => {
