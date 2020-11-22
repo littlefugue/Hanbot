70f2a3facde7bd91c8213740777d332802271415
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-22 23:29:50 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,6 @@ client.on('message', message => {
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
   const channel = member.guild.channels.cache.find(ch => ch.name === '잡담');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
   // Send the message, mentioning the member
   channel.send(`Welcome to the server`);
 });
