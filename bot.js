43ce7b759013251132d85a1bbc84369421720ab7
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-22 23:21:40 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,12 +24,11 @@ client.on('message', message => {
 });
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  // const channel = member.guild.channels.cache.find(ch => ch.name === '잡담');
+  const hichannel = member.guild.channels.cache.find(ch => ch.name === '잡담');
   // Do nothing if the channel wasn't found on this server
-  // if (!channel) return;
+  if (!hichannel) return;
   // Send the message, mentioning the member
-  //channel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
-  member.guild.channels.cache.get('762158660793008165').send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
+  hichannel.send(`${member}, 한강디코에 오신 것을 환영합니다. 잡담방에 인사+실명 남겨주시고 공지사항은 전부 읽어 주세요. 관리자가 확인 후 등급이 부여됩니다.`);
 });
 client.on('message', msg => {
   if (msg.content==='new'){
