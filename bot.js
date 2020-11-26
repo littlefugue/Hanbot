187518d5f1231b92a53ca759a9e275481498382f
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-26 23:52:30 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', msg => {
 			Sandbox.send('**명령어 목록**\n--일치 시 발동--\n> help!!!\n> 도움!!!\n> 갈!!!\n> 한강\n> ㅂ\n--포함 시 발동--\n> 경고!!!\n> 방장!!!');
 			break;
 		case('갈!!!'):
-			const attachment = new MessageAttachment('https://i.imgur.com/kDPRSj4.jpeg');
+			const attachment = new Discord.MessageAttachment('https://i.imgur.com/kDPRSj4.jpeg');
 			msg.channel.send(attachment);
 			break;
 		case('한강'):
