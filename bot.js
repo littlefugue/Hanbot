32e9d3d5b35eaec8d8514653a38114ea61afdfb4
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-27 00:38:08 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-//const { Client, MessageAttachment } = require('discord.js');
-
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
