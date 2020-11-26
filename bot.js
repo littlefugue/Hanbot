17f4ad3f3b16d398f31dbe3a1a85e581f651e198
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-26 17:11:58 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
 	const channel = member.guild.channels.cache.find(ch => ch.name === '잡담');
 	if (!channel) return;
-	channel.send(`${member}, 환영합니다. 잡담방에 실명 남겨주시고 공지방에 있는 규정을 읽어 주세요.\n관리자가 확인 후 등급이 부여됩니다.\n봇에 대한 도움말은 help! 혹은 도움!을 치세요.`);
+	channel.send(`${member}, 환영합니다. 잡담방에 실명 남겨주시고 공지방에 있는 규정을 읽어 주세요.\n관리자가 확인 후 등급이 부여됩니다.\n봇에 대한 도움말은 help!!! 혹은 도움!!!을 치세요.`);
 });
 
 client.on('message', msg => {
@@ -23,16 +23,13 @@ client.on('message', msg => {
 	const Hangang = client.guilds.cache.get('762158660112613376');
 	const Sandbox = client.channels.cache.get('772669318085345280')
 	switch (msg.content){
-		case('help!'):
-		case('도움!'):
+		case('help!!!'):
+		case('도움!!!'):
 			msg.reply('sandbox를 확인하세요');
-			Sandbox.send('**명령어 목록**\n--일치--\n> help!\n> 도움!\n> 경고!\n> 한강\n> ㅂ\n--포함--\n> 방장');
-			break;
-		case('경고!'):
-			msg.channel.send('실매매 얘기/어그로는 실매매방에서만 해주세요.');
+			Sandbox.send('**명령어 목록**\n--일치 시 발동--\n> help!!!\n> 도움!!!\n> 한강\n> ㅂ\n--포함 시 발동--\n> 경고!!!\n> 방장!!!');
 			break;
 		case('한강'):
-			msg.reply('가즈아!!!!!');
+			msg.reply('가즈아!!!');
 			break;
 		case('ㅂ'):
 			msg.channel.send('(╯°□°）╯︵ ┻━┻');
@@ -41,8 +38,11 @@ client.on('message', msg => {
 //			Sandbox.send(`${msg.member}hi.`);
 //			break;
 	}
-	if (msg.content.includes('방장')) {
-		msg.channel.send(`${Hangang.owner}!!!!!`);
+	if (msg.content.includes('경고!!!')) {
+		msg.channel.send('실매매 얘기/어그로는 실매매방에서만 해주세요.');
+	}
+	if (msg.content.includes('방장!!!')) {
+		msg.channel.send(`${Hangang.owner}!!!`);
 	}
 });
 
