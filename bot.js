1f2ea9e2212e30d5ca9c90b667338fc83dbdcb7c
littlefugue <65026095+littlefugue@users.noreply.github.com>
2020-11-26 15:55:46 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Hangang = client.guilds.cache.get('762158660112613376');
 //const { Client, MessageAttachment } = require('discord.js');
 
 client.on('ready', () => {
@@ -38,6 +37,7 @@ client.on('message', msg => {
 		case('방장!!!'):
 		case('방장!!!!'):
 		case('방장!!!!!'):
+			const Hangang = client.guilds.cache.get('762158660112613376');
 			msg.channel.send(`${Hangang.owner}!!!`)
 			break;
 	}
